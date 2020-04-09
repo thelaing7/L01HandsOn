d66498dcf2b96221238f6d2c0a07e16ceffbc1b9
thelaing7 <61952549+thelaing7@users.noreply.github.com>
2020-04-09 16:27:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function numberChecking(number1, number2){
+    if(number1 > 100){
+           console.log("number 1 has many digits!");
+
+    }
+    if(number1 < 0){
+          console.log("number 1 is a negative number");
+    }
+    if(number2 < 20){
+        console.log("Number 2 is not a very high number");
+    }
+    if(number2 > 50){
+        console.log("Number 2 is a high number");
+    }
+
+}
+numberChecking(450, 10);
+
